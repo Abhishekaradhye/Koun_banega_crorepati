f51768910dc3aa8c3ef9a5224e3d9adf50aafba5
Abhishek Aradhye <114404938+Abhishekaradhye@users.noreply.github.com>
2023-06-17 11:42:46 +0530

    Update kbc.py

--- kbc.py
+++ kbc.py
@@ -1,13 +1,15 @@
 questions = [['What is used to make ice-cream at home ?','Blackpaper powder','Custurd powder','coriender powder','Dry mango powder',2],\
         ['Which material is used in steel for hardness ?','Titanium','Aluminium','Carbon','Nickel',3],\
-        ["Who invented world's first ever electric bulb ?",'Thomas Edison','Graham Bell','Isaac Newton','Lionardo da Vinci',1]]
-levels = [10000,50000,120000]
+        ["Who invented world's first ever electric bulb ?",'Thomas Edison','Graham Bell','Isaac Newton','Lionardo da Vinci',1],\
+        ["According to Hinduism, how much years is the durtion of Kaliyuga ?",'1296000 years','864000 years','108000 years','432000 years',4]]
+levels = [10000,50000,120000,240000]
 print("Get set go ***")
 for i in range (len(questions)):
     question = questions[i]
     print(f"Question for Rs.{levels[i]}\n{question[0]}")
     print(f"1. {question[1]}     2. {question[2]}")
     print(f"3. {question[3]}     4. {question[4]}")
+    print(f"5. {question[5]})
     reply = int(input("Enter your option (1 - 4) : "))
     if reply == question[-1]:
         print(f"Your answer is correct !! Now you have won Rs.{levels[i]}\n")
@@ -17,6 +19,8 @@ for i in range (len(questions)):
             money = 50000
         elif i == 3:
             money = 120000
+        elif i == 4:
+            money = 240000
 
     else:
         print("You have given wrong answer, you were nice company !!!")
